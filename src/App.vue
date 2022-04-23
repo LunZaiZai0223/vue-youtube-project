200eedccda50814d1084fc4dfd0e6aa79dd8ace1
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2022-04-24 00:35:47 +0800

    feat: create basic api handlers

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,40 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+import { fetchVideosData } from './api/index.js';
+
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  mounted () {
+    this.getVideosData();
+    // fetchVideosData({ isFirstLoad: true });
+    // console.log('in mounted');
+    // const url = 'https://youtube.googleapis.com/youtube/v3/videos?';
+    // const apiKey = 'AIzaSyBZ7Mm1v1QEyUaYnPET_koH0uC10x3XWwo';
+    // // const config = {key: apiKey};
+    // const config = {
+    //   method: 'Get',
+    //   part: 'snippet',
+    //   chart: 'mostPopular',
+    //   maxResults: 12,
+    //   regionCode: 'TW',
+    //   key: apiKey,
+    // };
+    // // fetch(url).then((response) => response.json()).then((data) => console.log(data));
+    // fetch(url, config)
+    //   .then((response) => response.json())
+    //   .then((data) => console.log(data));
+  },
+  methods: {
+    async getVideosData () {
+      const { items, nextPageToken } = await fetchVideosData({ isFirstLoad: false, nextPageToken: 'CAwQAA' }).then((data) => data);
+      console.log(items);
+      console.log(nextPageToken);
+      console.log('hello world');
+    }
   }
 }
 </script>
